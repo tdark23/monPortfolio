9751c659bba96c63f18a64d9c966d6004ca8ab2e
Ted <ted.mbang@hintekk.com>
2022-02-22 23:53:58 +0100

    skills-completed

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React  from 'react';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 
 import './About.scss'
 
@@ -54,4 +54,8 @@ const About = () => {
   )
 }
 
-export default AppWrap(About, 'about');
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
