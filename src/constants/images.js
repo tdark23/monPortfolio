b1f8b3dc39e83b5728a9118a81663cbe0c37b8a1
tdark23 <89210225+tdark23@users.noreply.github.com>
2022-03-14 13:12:47 +0100

    Update images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -17,6 +17,7 @@ import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
+import vue from '../assets/next.png';
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
@@ -78,4 +79,5 @@ export default {
   nb,
   skype,
   spotify,
+  next,
 };
